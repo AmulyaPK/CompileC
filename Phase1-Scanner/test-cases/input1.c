c6d3533938523b0965991e6488139bd61e130cc3
Vanshika Mittal <vanshikavmittal@gmail.com>
2025-08-16 18:02:36 +0530

    Added lexical analysis for directives, comments and identifiers.

---
+++ Phase1-Scanner/test-cases/input1.c
@@ -0,0 +1,31 @@
+﻿#include <stdio.h>
+#define SIZE 5   // Array size
+
+/* Multi-line comment
+   describing the program */
+
+int globalVar = 10;   // Global variable
+
+// Function declaration
+void printMessage(const char *msg) {
+    printf("%s\n", msg);
+}
+
+// Function with array parameter
+int sumArray(int nums[], int size) {
+    int sum = 0;
+    for (int i = 0; i < size; i++) {
+        sum += nums[i];
+    }
+    return sum;
+}
+
+int main() {
+    int arr[SIZE] = {1, 2, 3, 4, 5};
+    printMessage("Hello World");
+
+    int localSum = sumArray(arr, SIZE);
+    printf("Sum = %d\n", localSum);
+
+    return 0;
+}
