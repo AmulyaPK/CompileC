99ddd5628b706aa5a7c6475820bb7d775300906c
Vanshika Mittal <vanshikavmittal@gmail.com>
2025-08-18 11:16:20 +0530

    Bug Fixes and Formatting Completed.

--- Phase1-Scanner/test-cases/input1.c
+++ Phase1-Scanner/test-cases/input1.c
@@ -1,7 +1,13 @@
-﻿#include<stdio.h>
+#include<stdio.h>
+int foo (int a, int b)
+{
+    return 0;
+}
+
 int main () {
     int x = 5;
     int y = 10;
+    int arr[10][50];
     int sum = x + y;
     printf("%d\n", sum);
     return 0;
